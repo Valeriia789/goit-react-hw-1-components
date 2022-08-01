b0fca4d251adc9e47b7341a8f40e1646d2c67821
Valeriia <valeriia.air@gmail.com>
2022-08-02 02:19:55 +0300

    інформація про юзера

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types'
+import { Stats } from './Stats'
+import {
+  Container,
+  Description,
+  Avatar,
+  UserName,
+  Line,
+  UserTag,
+  UserLocation
+} from './Profile.styled'
+
+export const Profile = ({ user: username, tag, location, avatar, stats }) => {
+  console.log(username)
+
+  return (
+    <Container>
+      <Description>
+        <Avatar src={avatar} alt='User avatar' />
+        <UserName>{username}</UserName>
+        <Line />
+        <UserTag>@{tag}</UserTag>
+        <UserLocation>{location}</UserLocation>
+
+        <Stats
+          followers={stats.followers}
+          views={stats.views}
+          likes={stats.likes}
+        />
+      </Description>
+    </Container>
+  )
+}
+
+Profile.propTypes = {
+  // user: PropTypes.exact ({ - прописуємо так, якщо 100% прийде те, що сказано
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.elementType.isRequired
+  })
+}
